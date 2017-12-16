0c91bc589ba5775a53b452f48d4552d5e93a4b73
s144842 <s.l.coenen@student.tue.nl>
2017-12-16 13:47:47 +0100

    Comment

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\s144842\documents\bestanden tue\jaar 3\kwartaal 2\web tech\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
